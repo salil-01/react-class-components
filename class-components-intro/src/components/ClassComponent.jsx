e091fb54fc8225dae80ade22964582d399afebf9
Salil Upadhyay <upsalil.01@gmail.com>
2023-06-12 16:18:21 +0530

    added access from public folder

--- class-components-intro/src/components/ClassComponent.jsx
+++ class-components-intro/src/components/ClassComponent.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./ClassComponent.module.css";
 class ClassComponent extends React.Component {
   render() {
-    return <h1 className={styles.heading}>Hii from Class Component</h1>;
+    return (
+      <>
+        <h1 className={styles.heading}>Hii from Class Component</h1>
+        {/* access anything from public folder */}
+        <img src="/vite.svg" alt="img" />
+      </>
+    );
   }
 }
 export default ClassComponent;
